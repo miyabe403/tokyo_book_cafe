cb79febbec2aefce6fbce0b7a02a1f83923f1b2a
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-10 20:03:13 +0900

    [Add] View の作成 管理者用 admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sesions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
