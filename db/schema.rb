2032aaa9057ee89c2ae633b7b9c19525386126a8
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-10 23:41:54 +0900

    [Add] Model の作成 Orderモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_10_140422) do
+ActiveRecord::Schema.define(version: 2023_10_10_142526) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -64,6 +73,19 @@ ActiveRecord::Schema.define(version: 2023_10_10_140422) do
     t.text "explanation", null: false
     t.integer "price", null: false
     t.boolean "is_sell", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "payment_method", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "delivery_charge", null: false
+    t.integer "total_price", null: false
+    t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
