defcfbd62167969ad4710efec6feb3d49a6bcbb0
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-10 21:50:06 +0900

    [Add] View の作成 管理者用 admin/customers/edit.html.erb

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class Admin::CustomersController < ApplicationController
+ 
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+end
