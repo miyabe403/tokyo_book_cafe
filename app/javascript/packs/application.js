2ee017994519e64081a00dcc126f8d306943d0db
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-10 16:09:45 +0900

    [Add] app/javascript/packs/application.js に記述を追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-// app/javascript/packs/application.js に記述を追記
+// app/javascript/packs/application.js に記述を追記 
 
 import "jquery";
 import "popper.js";
