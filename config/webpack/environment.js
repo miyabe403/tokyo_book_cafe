1c6d068c3378b1c817c1273ace19c6762ae4c4c0
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-10 15:58:05 +0900

    [Add] config/webpack/environment.js に記述を追記

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,7 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-// config/webpack/environment.js に記述を追記 
+// config/webpack/environment.js に記述を追記
 
 const webpack = require('webpack')
 environment.plugins.prepend(
