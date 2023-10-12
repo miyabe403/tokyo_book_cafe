6c47402ddfdb03bda6d83a43e54371d26b4781d8
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-12 18:35:37 +0900

    [Add] Model にバリデーションを設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,15 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # バリデーションを設定
+  # validatesで対象とする項目を指定し、入力されたデータの
+  # presence（存在）をチェックします。 trueと記述すると、
+  # データが存在しなければならないという設定になります。
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  
 end
