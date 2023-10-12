28534e7fe976f8724940a90726fb0bad60e16518
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-13 03:17:36 +0900

    [Add] Controller の追記 public/itemsコントローラ

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,20 @@
 class Public::ItemsController < ApplicationController
   def index
+    if params[:genre_id]
+      genre = Genre.find(params[:genre_id])
+      @items = genre.items
+    else
+      @items = Item.page(params[:page]).per(8)
+    end
+
+    @all_items = Item.all.page(params[:page]).per(8)
+    @genres = Genre.all
   end
   
   def show
+    @item = Item.find(params[:id])
+    #@price = @item.price_without_tax
+    @genres = Genre.all
+    @cart_item = CartItem
   end
 end
