6230f580d1b92ba7ad3b9a059c09b18064ca6da8
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-13 12:15:08 +0900

    [Add] Model にアソシエーションを設定

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,12 @@
 class OrderDetail < ApplicationRecord
+  # アソシエーションを設定
+  belongs_to :order
+  belongs_to :item
+  
+  enum production_status: {
+      untouched: 0,
+      waiting: 1,
+      working: 2,
+      completed: 3
+  }
 end
