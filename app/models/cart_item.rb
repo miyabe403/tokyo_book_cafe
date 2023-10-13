edea39d4cd9dd36bef651f6fb5620a8b7107406a
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-13 10:53:09 +0900

    [Add] Model に合計金額を表示させる処理を追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,7 +3,7 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
   
-  # 合計金額を表示させる処理
+  # 合計金額を表示させる処理 
   # モデルに subtotal (小計) メソッドを定義
   # 税込価格と数量をかけて算出
   def subtotal
