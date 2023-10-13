c7eb39c413de945d04513046be99c63207546254
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-13 20:44:10 +0900

    [Add] Model に請求額を求めるメソッドを追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,19 @@ class Order < ApplicationRecord
   
   enum payment_method: { credit_card: 0, transfer: 1 }
 
+  # enum の設定
+  # 注文ステータス
   enum status: {
-    waiting: 0,
-    paid_up: 1,
-    making: 2,
-    preparing: 3,
-    shippend: 4
+    waiting: 0,     # waiting (入金待ち)  例 : waiting_payment（入金待ち）
+    paid_up: 1,     # paid_up (支払い済み)例 : confirm_payment（入金確認）
+    making: 2,      # making (制作中) 　　例 : shipped（出荷済み）
+    preparing: 3,   # pewparing (準備中)  例 : out_for_delivery（配送中）
+    shippend: 4     # shippend (配達済み) 例 : delivered（配達済み）
 
   }
+  
+  # 請求額を求めるメソッド
+  def billing_amount
+    self.total_price + self.delivery_charge
+  end
 end
