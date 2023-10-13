5802b92bcb02e50e448a995c6495de605e9c8a7a
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-13 10:57:18 +0900

    [Add] Model にアソシエーションを設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # アソシエーションを設定
+  has_many :cart_items, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # バリデーションを設定 
