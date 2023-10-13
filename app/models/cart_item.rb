0f87e01a285120f35ba73656ce16bb7cb0a85e5f
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-10-13 10:54:01 +0900

    [Add] Model に subtotal (小計) メソッドを定義

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,7 +4,7 @@ class CartItem < ApplicationRecord
   belongs_to :item
   
   # 合計金額を表示させる処理 
-  # モデルに subtotal (小計) メソッドを定義
+  # モデルに subtotal (小計) メソッドを定義 
   # 税込価格と数量をかけて算出
   def subtotal
     item.with_tax_price * amount
